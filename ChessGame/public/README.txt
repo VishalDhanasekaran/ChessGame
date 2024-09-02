5c659dc3b060d146b1bf7d0942ad651f43885ca8
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-03 00:40:28 +0530

    misc changes

--- ChessGame/public/README.txt
+++ ChessGame/public/README.txt
@@ -1,5 +1,4 @@
 Kosal piece set
-
 Author: Philatype (https://github.com/philatype)
 License: CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
 Source: https://sharechess.github.io
--- ChessGame/src/helper.js
+++ ChessGame/src/helper.js
@@ -1,32 +1,32 @@
-export const getChar = number => String.fromCharCode(number+96);
+export const getChar = (number) => String.fromCharCode(number + 96);
 
 export const createPosition = () => {
+  const position = new Array(8).fill("").map((x) => new Array(8).fill(""));
+  // if(defaultVal === "White"){
+  console.log("Option white");
+  position[0][0] = "rw";
 
- const position = new Array(8).fill('').map(x=> new Array(8).fill(''));
-// if(defaultVal === "White"){
-console.log("Option white")
- position[0][0] = 'rw'
- position[0][7] = 'rw'
- position[7][0] = 'rb'
- position[7][7] = 'rb'
- position[0][1]= 'nw'
- position[0][6]= 'nw'
- position[7][1]= 'nb'
- position[7][6]= 'nb'
- position[0][2]= 'bw'
- position[0][5]= 'bw'
- position[7][2]= 'bb'
- position[7][5]= 'bb'
- position[0][3]= 'qw'
- position[0][4]= 'kw'
- position[7][3]= 'kb'
- position[7][4]= 'qb'
+  position[0][7] = "rw";
+  position[7][0] = "rb";
+  position[7][7] = "rb";
+  position[0][1] = "nw";
+  position[0][6] = "nw";
+  position[7][1] = "nb";
+  position[7][6] = "nb";
+  position[0][2] = "bw";
+  position[0][5] = "bw";
+  position[7][2] = "bb";
+  position[7][5] = "bb";
+  position[0][3] = "qw";
+  position[0][4] = "kw";
+  position[7][3] = "kb";
+  position[7][4] = "qb";
 
- for (let index = 0; index < 8; index++) {
-    position[1][index] = 'pw';
-    position[7-1][index] = 'pb';    
- }
-/*}else  {
+  for (let index = 0; index < 8; index++) {
+    position[1][index] = "pw";
+    position[7 - 1][index] = "pb";
+  }
+  /*}else  {
 
 console.log(defaultVal+'')
 position[0][0] = 'rb'
@@ -48,15 +48,14 @@ position[7][4]= 'qw'
 
 for (let index = 0; index < 8; index++) {
    position[1][index] = 'pb';
-   position[7-1][index] = 'pw';    
+   position[7-1][index] = 'pw';
 }
 }*/
-console.log(position)
-return position;
-}
+  console.log(position);
+  return position;
+};
 
 export const copyPosition = (oldposition) => {
-   
-   console.log(oldposition.slice() === oldposition)
-   return oldposition.slice()
-}
+  console.log(oldposition.slice() === oldposition);
+  return oldposition.slice();
+};
