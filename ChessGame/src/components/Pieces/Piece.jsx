7c85830815352d08117fcfed16aca3de87f9b77c
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-03 21:36:55 +0530

    update piece.js to incorporate context and reducer patterns

--- ChessGame/src/components/Pieces/Piece.jsx
+++ ChessGame/src/components/Pieces/Piece.jsx
@@ -1,22 +1,22 @@
+const Piece = ({ rank, file, piece }) => {
+  const onDragStart = (e) => {
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/plain", `${piece},${rank},${file}`);
 
-
-
-
-const Piece = ({rank, file, piece}) => {
-    const onDragStart = e =>{
-        e.dataTransfer.effectAllowed = 'move'
-        e.dataTransfer.setData('text/plain',`${piece},${rank},${file}`)
-
-        setTimeout(()=>{
-            e.target.style.display = 'none'
-        },0)
-
-    }
-    const onDragEnd = e => {
-            e.target.style.display = 'block'
-    }
-    return (
-         <div className={`piece ${piece} p-${file}${rank}`} draggable={true} onDragStart={onDragStart} onDragEnd={onDragEnd}/>
-        )
-}
-export default Piece
+    setTimeout(() => {
+      e.target.style.display = "none";
+    }, 0);
+  };
+  const onDragEnd = (e) => {
+    e.target.style.display = "block";
+  };
+  return (
+    <div
+      className={`piece ${piece} p-${file}${rank}`}
+      draggable={true}
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+    />
+  );
+};
+export default Piece;
