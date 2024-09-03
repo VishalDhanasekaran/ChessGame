1d24c275a430e7b4a8487c2a278fe65b92cfe66b
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-03 21:35:50 +0530

    update app.jsx for context and reducer implementation

--- ChessGame/src/App.jsx
+++ ChessGame/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import Board from './components/Board/Board'
-import ToggleButton from './components/ToggleButton/TogggleButton'
-import './App.css'
+import { useState } from "react";
+import Board from "./components/Board/Board";
+import ToggleButton from "./components/ToggleButton/TogggleButton";
+import "./App.css";
+import AppContext from "./contexts/Context";
+import { useReducer } from "react";
+import { reducer } from "./reducer/reducer";
+import { initGameState } from "./constant";
 
 function App() {
-
+  const [appState, dispatch] = useReducer(reducer, initGameState);
+  const providerState = {
+    appState,
+    dispatch,
+  };
   return (
-    <div className='App'>
-      <Board />
-    </div>
-  )
+    <AppContext.Provider value={providerState}>
+      <div className="App">
+        <Board />
+      </div>
+    </AppContext.Provider>
+  );
 }
 
-export default App
+export default App;
