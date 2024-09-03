2a483d9fe7bb900c7b9f9c2292479aa8c42dc63a
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-03 21:38:22 +0530

    misc changes

--- ChessGame/src/helper.js
+++ ChessGame/src/helper.js
@@ -26,32 +26,7 @@ export const createPosition = () => {
     position[1][index] = "pw";
     position[7 - 1][index] = "pb";
   }
-  /*}else  {
-
-console.log(defaultVal+'')
-position[0][0] = 'rb'
-position[0][7] = 'rb'
-position[7][0] = 'rw'
-position[7][7] = 'rw'
-position[0][1]= 'nb'
-position[0][6]= 'nb'
-position[7][1]= 'nw'
-position[7][6]= 'nw'
-position[0][2]= 'bb'
-position[0][5]= 'bb'
-position[7][2]= 'bw'
-position[7][5]= 'bw'
-position[0][3]= 'kb'
-position[0][4]= 'qb'
-position[7][3]= 'kw'
-position[7][4]= 'qw'
-
-for (let index = 0; index < 8; index++) {
-   position[1][index] = 'pb';
-   position[7-1][index] = 'pw';
-}
-}*/
-  console.log(position);
+  console.log("POSITION:", position);
   return position;
 };
 
