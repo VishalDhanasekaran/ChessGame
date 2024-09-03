0d249161448061c910aff5d26a0665e72258bfa6
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-03 21:34:21 +0530

    implement context to pass chess moves to all comonents

---
+++ ChessGame/src/contexts/Context.js
@@ -0,0 +1,7 @@
+import { useContext, createContext } from "react";
+
+const AppContext = createContext();
+export function useAppContext() {
+  return useContext(AppContext);
+}
+export default AppContext;
