da41c072d7216c9350bcc2d7658c7c2efad4a66c
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-04 09:34:18 +0530

    misc changes

--- ChessGame/src/reducer/actions/move.js
+++ ChessGame/src/reducer/actions/move.js
@@ -1,7 +1,6 @@
 import actionTypes from "../actionTypes";
 
 export const makeNewMove = ({ newPosition }) => {
-  console.log("NEWPOSITIONATMAKENEWMOVE", newPosition);
   return {
     type: actionTypes.NEW_MOVE,
     payload: { newPosition },
