b1f7e4300c173af0f06d4f34999bb65a9965542c
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-08 01:36:44 +0530

    add rules for each type of coin

--- ChessGame/src/components/Pieces/Piece.jsx
+++ ChessGame/src/components/Pieces/Piece.jsx
@@ -1,4 +1,17 @@
+import arbiter from "../../arbiter/arbiter";
+import { useAppContext } from "../../contexts/Context";
+import { generateCandidateMoves } from "../../reducer/actions/move";
+
+
 const Piece = ({ rank, file, piece }) => {
+
+  const {appState, dispatch } = useAppContext();
+
+  const {turn , position } = appState;
+  const currentPosition = position[position.length -1 ]
+
+  
+
   const onDragStart = (e) => {
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text/plain", `${piece},${rank},${file}`);
@@ -6,6 +19,13 @@ const Piece = ({ rank, file, piece }) => {
     setTimeout(() => {
       e.target.style.display = "none";
     }, 0);
+
+    if (turn === piece[1]){
+    console.log("piece: ",piece[1]," turn : ",turn)
+        const CandidateMoves = arbiter.getRegularMoves({position:currentPosition,piece:piece, rank, file})
+        console.log("Candidate Moves" ,CandidateMoves)
+        dispatch(generateCandidateMoves(CandidateMoves))
+    } 
   };
   const onDragEnd = (e) => {
     e.target.style.display = "block";
