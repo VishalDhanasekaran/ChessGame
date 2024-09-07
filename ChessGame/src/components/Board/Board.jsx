915848e72208958729428fd7045607bf90bf6140
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-08 01:35:57 +0530

    update board state to display candidate moves

--- ChessGame/src/components/Board/Board.jsx
+++ ChessGame/src/components/Board/Board.jsx
@@ -2,14 +2,26 @@ import './Board.css'
 import Files from './../Bits/Files'
 import Ranks from '../Bits/Ranks'
 import Pieces from '../Pieces/Pieces'
+import { useAppContext } from '../../contexts/Context'
 const Board = () => {
 
     const ranks = Array(8).fill().map((x,i) => 8-i)
     const files = Array(8).fill().map((x,i) => i+1) 
+    const {appState } = useAppContext()
+    const position = appState.position[appState.position.length -1]
+    console.log("POSITION",position)
     const getTileClassName = (i,j) => {
-        return (
-            ((i+j)%2===0)?"tile--dark":"tile--light"
-        )
+        let c = ((i+j)%2===0)?" tile--dark ":" tile--light "
+
+        if (appState.candidateMoves?.find(m => m[0] == i && m[1] == j)){
+            if (position[i][j]){
+                c += ' attacking '
+            }else{
+                c += ' highlighting '
+            }
+        }
+        return c
+        
     }
     return (
         <div className="Board">
@@ -21,7 +33,7 @@ const Board = () => {
                             files.map(
                                 (file,j) => 
                                     <div key={ rank + "-" + file }
-                                        className={getTileClassName(9-i,j)}>{/*{rank}{file}*/}</div>
+                                        className={getTileClassName(7-i,j)}>{/*{rank}{file}*/}</div>
                                 
                             )
                         )
