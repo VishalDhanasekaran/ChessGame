f617f184c8c42dc05c5082ec26f6b5249e9e100e
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-08 01:39:27 +0530

    misc changes

--- ChessGame/src/helper.js
+++ ChessGame/src/helper.js
@@ -4,27 +4,26 @@ export const createPosition = () => {
   const position = new Array(8).fill("").map((x) => new Array(8).fill(""));
   // if(defaultVal === "White"){
   console.log("Option white");
-  position[0][0] = "rw";
-
-  position[0][7] = "rw";
-  position[7][0] = "rb";
-  position[7][7] = "rb";
-  position[0][1] = "nw";
-  position[0][6] = "nw";
-  position[7][1] = "nb";
-  position[7][6] = "nb";
-  position[0][2] = "bw";
-  position[0][5] = "bw";
-  position[7][2] = "bb";
-  position[7][5] = "bb";
-  position[0][3] = "qw";
-  position[0][4] = "kw";
-  position[7][3] = "kb";
-  position[7][4] = "qb";
+  position[0][0] = "rW";
+  position[0][7] = "rW";
+  position[7][0] = "rB";
+  position[7][7] = "rB";
+  position[0][1] = "nW";
+  position[0][6] = "nW";
+  position[7][1] = "nB";
+  position[7][6] = "nB";
+  position[0][2] = "bW";
+  position[0][5] = "bW";
+  position[7][2] = "bB";
+  position[7][5] = "bB";
+  position[0][3] = "qW";
+  position[0][4] = "kW";
+  position[7][3] = "kB";
+  position[7][4] = "qB";
 
   for (let index = 0; index < 8; index++) {
-    position[1][index] = "pw";
-    position[7 - 1][index] = "pb";
+    position[1][index] = "pW";
+    position[7 - 1][index] = "pB";
   }
   return position;
 };
