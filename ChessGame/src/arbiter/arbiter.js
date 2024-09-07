a38b7ae2fe1b5da9ed9238212a24e85792c3f97f
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-09-08 01:33:05 +0530

    update arbiter for selecting candidate moves

---
+++ ChessGame/src/arbiter/arbiter.js
@@ -0,0 +1,24 @@
+import { getKingMoves, getPawnMoves, getQueenMoves, getBishopMoves, getKnightMoves, getRookMoves } from "./getMoves"
+
+const arbiter = {
+    getRegularMoves : function({position, piece , rank , file })  {
+        console.log(piece)
+        switch(piece[0]){
+            case 'k':
+              return getKingMoves({position,rank,file}) 
+            case 'p':
+              return getPawnMoves({position,rank,file}) 
+            case 'q':
+                return getQueenMoves({position , rank , file });
+            case 'n':
+                return getKnightMoves({position , rank , file });
+            case 'b':
+                return getBishopMoves({position,rank,file})
+            default:
+                return getRookMoves({position , piece, rank , file });
+            
+        }
+    }
+}
+
+export default arbiter;
