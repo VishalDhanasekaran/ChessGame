4e4ceef697db0e092c3d4e43eef178110875de4f
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-10-14 22:34:57 +0530

    define structure of promotion popup box

---
+++ ChessGame/src/components/Popup/Promotion/Promotion.jsx
@@ -0,0 +1,61 @@
+import "./Promotion.css";
+import "./../Popup.css";
+import { useAppContext } from "../../../contexts/Context";
+import { copyPosition } from "../../../helper";
+import {
+  clearCandidateMoves,
+  makeNewMove,
+} from "../../../reducer/actions/move";
+
+const Promotion = ({ closePopupCallback }) => {
+  const options = ["q", "r", "b", "n"];
+  const { appState, dispatch } = useAppContext();
+  const { promotionSquare } = appState;
+  if (!promotionSquare) {
+    return;
+  }
+  const color = promotionSquare.x === 7 ? "W" : "B";
+  const determinePromotionBoxPosition = () => {
+    const style = {};
+    if (promotionSquare.x === 7) {
+      style.top = "-12.5%";
+    } else {
+      style.top = "97.5%";
+    }
+    if (promotionSquare.y <= 1) {
+      style.left = "0%";
+    } else if (promotionSquare.y >= 6) {
+      style.right = "0%";
+    } else {
+      style.left = `${12.5 * promotionSquare.y - 20}%`;
+    }
+    return style;
+  };
+
+  const applySelection = (option) => () => {
+    closePopupCallback();
+    const newPosition = copyPosition(
+      appState.position[appState.position.length - 1],
+    );
+    newPosition[promotionSquare.rank][promotionSquare.file] = "";
+    newPosition[promotionSquare.x][promotionSquare.y] = `${option}${color}`;
+    dispatch(clearCandidateMoves());
+    dispatch(makeNewMove({ newPosition }));
+  };
+  return (
+    <div
+      className="popup--inner promotion-choices"
+      style={determinePromotionBoxPosition()}
+    >
+      {options.map((option) => (
+        <div
+          className={`piece ${option}${color}`}
+          onClick={applySelection(option)}
+          key={option}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Promotion;
