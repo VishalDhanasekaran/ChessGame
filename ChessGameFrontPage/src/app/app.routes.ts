0b78801f2fca98e4df01372bf7cc61656e0c8951
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-10-14 22:39:09 +0530

    fix and update routes to play area

--- ChessGameFrontPage/src/app/app.routes.ts
+++ ChessGameFrontPage/src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { Routes } from '@angular/router';
-import { HeaderBannerComponent } from './header-banner/header-banner.component';
-import {NotFoundComponent} from './not-found/not-found.component'
+import { Routes } from "@angular/router";
+import { HeaderBannerComponent } from "./header-banner/header-banner.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-import {PlaceHolderComponent} from './place-holder/place-holder.component'
+import { PlaceHolderComponent } from "./place-holder/place-holder.component";
+import { RedirectToPlayComponentComponent } from "./redirect-to-play-component/redirect-to-play-component.component";
 export const routes: Routes = [
-    {path:'home',component:HeaderBannerComponent},
-    {path:'NEWS',component:PlaceHolderComponent},
-    {path:'LIVE_SCOREBOARD',component:PlaceHolderComponent},
-    {path:'PLAY',component:PlaceHolderComponent},
-    {path:'**',component:NotFoundComponent}
+  { path: "home", component: HeaderBannerComponent },
+  { path: "", component: HeaderBannerComponent },
+  { path: "NEWS", component: PlaceHolderComponent },
+  { path: "LIVE_SCOREBOARD", component: PlaceHolderComponent },
+  { path: "PLAY", component: RedirectToPlayComponentComponent },
+  { path: "**", component: NotFoundComponent },
 ];
