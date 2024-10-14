a9711cf9a28a14c40c0e0cc6ed8371e8910405b3
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-10-14 22:35:30 +0530

    update initial gamestate

--- ChessGame/src/constant.js
+++ ChessGame/src/constant.js
@@ -1,7 +1,14 @@
 import { createPosition } from "./helper";
-
+export const Status = {
+  ongoing: "Ongoing",
+  promoting: "Promoting",
+  white: "White is  victorious",
+  black: "Black is victorious",
+};
 export const initGameState = {
   position: [createPosition()],
   turn: "W",
-  candidateMoves : []
+  candidateMoves: [],
+  status: Status.ongoing,
+  promotionSquare: null,
 };
