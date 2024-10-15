2fd84d42dfe248d25684079e3dc2a6e5a6275e3e
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-10-15 11:16:38 +0530

    update castle moves

--- ChessGame/src/arbiter/move.js
+++ ChessGame/src/arbiter/move.js
@@ -11,6 +11,17 @@ export const movePawn = ({ position, piece, rank, file, x, y }) => {
 };
 export const movePiece = ({ position, piece, rank, file, x, y }) => {
   const newPosition = copyPosition(position);
+
+  if (piece.startsWith("k") && Math.abs(y - file) > 1) {
+    if (y === 2) {
+      newPosition[rank][0] = "";
+      newPosition[rank][3] = piece.endsWith("W") ? "rW" : "rB";
+    }
+    if (y === 6) {
+      newPosition[rank][7] = "";
+      newPosition[rank][5] = piece.endsWith("W") ? "rW" : "rB";
+    }
+  }
   newPosition[rank][file] = "";
   newPosition[x][y] = piece;
   return newPosition;
